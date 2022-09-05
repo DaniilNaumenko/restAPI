cdc80f1fd52cf0c07266be0e895f72a002714882
DaniilNaumenko <danilanaumenko2000@yandex.ru>
2022-09-05 20:38:04 +0400

    last steps

--- event-service-rest/src/main/java/by/naumenka/eventservicerest/SwaggerConfig.java
+++ event-service-rest/src/main/java/by/naumenka/eventservicerest/SwaggerConfig.java
@@ -3,16 +3,12 @@ package by.naumenka.eventservicerest;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.hateoas.client.LinkDiscoverer;
-import org.springframework.hateoas.client.LinkDiscoverers;
-import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
-import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @EnableSwagger2
 @Configuration
@@ -22,13 +18,14 @@ public class SwaggerConfig {
     public Docket apiV1() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
+                .apis(RequestHandlerSelectors.basePackage("by.naumenka.eventservicerest"))
                 .build();
     }
 
     @Bean
-    public LinkDiscoverers discoverers() {
-        List<LinkDiscoverer> plugins = new ArrayList<>();
-        plugins.add(new CollectionJsonLinkDiscoverer());
-        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+    UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .defaultModelsExpandDepth(-1)
+                .build();
     }
 }
